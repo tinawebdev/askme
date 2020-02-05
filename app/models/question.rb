69f80fb7831366022747aa5f1c3defdd99151d11
Kristina Goncharova <kris.goncharova@inbox.ru>
2020-02-05 21:38:24 +0300

    fix question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :user
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', optional: true
 
   validates :text, presence: true, length: { maximum: 255 }
 end
