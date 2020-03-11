96333410495f43da6076bd334d7dc8189326f874
Kristina Goncharova <kris.goncharova@inbox.ru>
2020-03-11 12:53:19 +0300

    Localization added

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module Askme
 
     config.time_zone = 'Moscow'
 
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :ru
     config.i18n.locale = :ru
 
     config.i18n.fallbacks = [:en]
